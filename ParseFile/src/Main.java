0363030462cd885243a51e9d47bf77e8a63ee1ab
mouna <mouna@DESKTOP-UAD8M32>
2018-04-23 17:12:38 +0200

    test

--- ParseFile/src/Main.java
+++ ParseFile/src/Main.java
@@ -11,7 +11,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		try {
-			File file = new File("C:\\Users\\mouna\\eclipse-workspace\\ParseFile\\src\\data");
+			File file = new File("C:\\Users\\mouna\\git\\ParseFile\\ParseFile\\src\\data");
 			FileReader fileReader = new FileReader(file);
 			BufferedReader bufferedReader = new BufferedReader(fileReader);
 			StringBuffer stringBuffer = new StringBuffer();
@@ -24,7 +24,7 @@ public class Main {
 				//
 			}
 			
-			BufferedWriter bwr = new BufferedWriter(new FileWriter(new File("C:\\Users\\mouna\\eclipse-workspace\\ParseFile\\src\\demo")));
+			BufferedWriter bwr = new BufferedWriter(new FileWriter(new File("C:\\Users\\mouna\\git\\ParseFile\\ParseFile\\src\\demo")));
 			
 			//write contents of StringBuffer to a file
 			bwr.write(stringBuffer.toString());
@@ -43,7 +43,7 @@ public class Main {
 		}
 	
 		try {
-			File file = new File("C:\\Users\\mouna\\eclipse-workspace\\ParseFile\\src\\demo");
+			File file = new File("C:\\Users\\mouna\\git\\ParseFile\\ParseFile\\src\\demo");
 			FileReader fileReader = new FileReader(file);
 			BufferedReader bufferedReader = new BufferedReader(fileReader);
 			StringBuffer stringBuffer = new StringBuffer();
@@ -55,7 +55,7 @@ public class Main {
 				stringBuffer.append("\n\n");
 			}
 			
-			BufferedWriter bwr = new BufferedWriter(new FileWriter(new File("C:\\Users\\mouna\\eclipse-workspace\\ParseFile\\src\\demo2")));
+			BufferedWriter bwr = new BufferedWriter(new FileWriter(new File("C:\\Users\\mouna\\git\\ParseFile\\ParseFile\\src\\demo2")));
 			
 			//write contents of StringBuffer to a file
 			bwr.write(stringBuffer.toString());
@@ -72,6 +72,39 @@ public class Main {
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+		
+		try {
+			File file = new File("C:\\Users\\mouna\\git\\ParseFile\\ParseFile\\src\\finalfile");
+			FileReader fileReader = new FileReader(file);
+			BufferedReader bufferedReader = new BufferedReader(fileReader);
+			StringBuffer stringBuffer = new StringBuffer();
+			String line;
+			while ((line = bufferedReader.readLine()) != null) {
+			String newline=breakString(line); 
+				 newline="('"+newline+"'),"; 
+				stringBuffer.append(newline);
+				stringBuffer.append("\n\n");
+			}
+			
+			BufferedWriter bwr = new BufferedWriter(new FileWriter(new File("C:\\Users\\mouna\\git\\ParseFile\\ParseFile\\src\\classdatabasefile")));
+			
+			//write contents of StringBuffer to a file
+			bwr.write(stringBuffer.toString());
+			
+			//flush the stream
+			bwr.flush();
+			
+			//close the stream
+			bwr.close();
+			fileReader.close();
+			System.out.println("Contents of file:");
+			System.out.println(stringBuffer.toString());
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		
 	}
 	
 	
